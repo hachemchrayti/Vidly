d9478f17e08f0bca7544b2eb505f45627a1b67ca
Hachem Chrayti <hachem.chrayti@outlook.fr>
2021-03-10 21:05:20 +0100

    Add Migrations with Seed MembershipTypes & Use annotations in Customer Model/Database Table

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
 using Vidly.Models;
 
@@ -6,29 +7,34 @@ namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
+        private ApplicationDbContext _context;
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+           _context.Dispose();
+        }
+
         // GET: Customers
         public ActionResult Index()
         {
-            List<Customer> customers = new List<Customer>()
-            {
-                new Customer{ Id=1,Name="Sergio"},
-                new Customer{ Id=2,Name="Karim"}
-
-            };
+            var customers = _context.Customers;
             return View(customers);
         }
 
         public ActionResult Details(int id)
         {
-            if (id == 1)
-            {
-                return View(new Customer() { Id = 1, Name = "Sergio" });
-            }
-            else
-            {
-                return View(new Customer() { Id = 2, Name = "Karim" });
-            }
+            var customers = _context.Customers;
+            var customer = customers.SingleOrDefault(c=>c.Id==id);
 
+            if (customer == null) return HttpNotFound();
+            return View(customer);
         }
+
+       
+
     }
 }
