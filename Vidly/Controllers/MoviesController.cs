41826bdf29b957d5d12f44045e90fbf89a088d0a
Hachem Chrayti <hachem.chrayti@outlook.fr>
2021-03-12 14:40:05 +0100

    ADD client & server sde validation for  create new movie form

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -92,9 +92,8 @@ namespace Vidly.Controllers
             if (movie == null)
                 return HttpNotFound();
 
-            var viewModel = new MovieFormViewModel
+            var viewModel = new MovieFormViewModel(movie)
             {
-                Movie = movie,
                 Genres = _context.Genres.ToList()
             };
 
